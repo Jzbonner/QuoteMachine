a6c6b72c979d9f840f5a90e4c7041fe879dbbb96
Jzbonner <jarrett.bonner@gmail.com>
2018-04-15 08:51:16 -0400

    changes to QM functionality

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-$(document).ready(function() {
-    function getQuote() {
-        $.ajax({
-            url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-            success: function(data) {
-              var post = data.shift();
-              $('#quote-title').text(post.title);
-              $('#quote-content').html(post.content);
-            }
-        });
-        
-        cache: false;
-    };
-
-    //initialize the first quote to appear prior to the Action Event.
-    getQuote(); 
-
-    // Action Event to trigger the generation of a new quote. 
-    $("#generate-quote").on("click", function () {
-        getQuote(); 
+function getQuote() {
+    $.ajax({
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        success: function(data) {
+            var post = data.shift();
+            $('#quote-title').html('<i class="far fa-arrow-alt-circle-right"></i>   ' + post.title);
+            $('#quote-content').html(post.content);
+        }
     });
+    cache: false;
+};
+
+//initialize the first quote to appear prior to the Action Event.
+getQuote(); 
+
+// Action Event to trigger the generation of a new quote. 
+$("#generate-quote").on("click", function () {
+    cache: false; 
+    getQuote(); 
 });
