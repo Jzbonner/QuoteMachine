9b2486ce2b93490f5b0775c237f34d0c8fa85d60
Jzbonner <jarrett.bonner@gmail.com>
2018-04-15 11:23:39 -0400

    fine tuning of Quote Machine app

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
 function getQuote() {
+    var url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+    $.ajaxSetup({ cache: false }); // necessary to actually provide a new quote each time this function is called
     $.ajax({
-        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        url: url,
         success: function(data) {
-            var post = data.shift();
-            $('#quote-title').html('<i class="far fa-arrow-alt-circle-right"></i>   ' + post.title);
-            $('#quote-content').html(post.content);
+            var post = data.pop();
+            var $title = $('#quote-title').html('<i class="far fa-arrow-alt-circle-right"></i>   ' + post.title);
+            var $content = $('#quote-content').html(post.content);
         }
     });
-    cache: false;
+
+    document.getElementById('quote-title').innerHTML = $title; 
+    document.getElementById('quote-content').innerHTML = $content; 
 };
 
 //initialize the first quote to appear prior to the Action Event.
 getQuote(); 
 
-// Action Event to trigger the generation of a new quote. 
-$("#generate-quote").on("click", function () {
-    cache: false; 
-    getQuote(); 
-});
+// twitter functionality 
+function tweetQuote() {
+    var quoteToTweet = $(".quote").text();
+    var authorToTweet = $(".author").text(); 
+    var myURL = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(quoteToTweet + authorToTweet);
+    window.open(myURL, 'twitter');
+    return false; 
+};
+
+
